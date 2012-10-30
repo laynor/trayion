f1ae627527305165fdcf508bbcc16872b7e34073
Alessandro Piras <laynor@gmail.com>
2012-10-30 12:53:08 +0100

    Added an sh script to launch trayion in a while true loop, so that it is restarted when it crashes.

---
+++ trayion-crash-kludge.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script is a kludge to restart trayion in case it crashes.
+# This is UGLY, but I did not have the time to properly fix it,
+# and it does its dirty job.
+
+while true; do
+  trayion
+done
